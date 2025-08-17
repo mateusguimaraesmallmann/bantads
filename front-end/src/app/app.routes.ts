b3b4810be5f5102ac4d157afc4a638e82670035e
Marcel Zimmer <Marcel-zimmer>
2025-08-17 19:22:27 -0300

    iniciado a implementação do formulario de atualizar usuário

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ClientHomeComponent } from './features/client-home/client-home.component';
+import { UpdateProfileComponent } from './features/update-profile/update-profile.component';
 
 export const routes: Routes = [
-  { path: 'client-home', component: ClientHomeComponent }
+  { path: 'client-home', component: ClientHomeComponent },
+  { path: 'update-profile', component: UpdateProfileComponent }
 ];
