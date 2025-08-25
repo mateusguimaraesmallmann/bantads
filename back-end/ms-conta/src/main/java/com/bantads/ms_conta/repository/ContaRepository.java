ebba1092dc9a006180baa790f6cb861eaec249fe
Ariane Souza <ari.souza02@outlook.com>
2025-08-25 16:23:37 -0300

    feat[MS-Conta]: Adicionando repositories

---
+++ back-end/ms-conta/src/main/java/com/bantads/ms_conta/repository/ContaRepository.java
@@ -0,0 +1,14 @@
+package com.bantads.ms_conta.repository;
+
+import com.bantads.ms_conta.model.entity.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+
+    Optional<Conta> findByNumero(String numero);
+
+    boolean existsByNumero(String numero);
+
+}
---
+++ back-end/ms-conta/src/main/java/com/bantads/ms_conta/repository/MovimentacaoRepository.java
@@ -0,0 +1,8 @@
+package com.bantads.ms_conta.repository;
+
+import com.bantads.ms_conta.model.entity.Movimentacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+
+}
