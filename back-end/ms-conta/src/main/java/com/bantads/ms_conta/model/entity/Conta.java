5350628b8b43248587bdc0e2fde9a61d0719de05
Ariane Souza <ari.souza02@outlook.com>
2025-08-25 16:22:46 -0300

    feat[MS-Conta]: Adicionando entidades conta e movimentacao

---
+++ back-end/ms-conta/src/main/java/com/bantads/ms_conta/model/entity/Conta.java
@@ -0,0 +1,46 @@
+package com.bantads.ms_conta.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "conta")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 4)
+    private String numero;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal saldo;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal limite;
+
+    @Column(nullable = false)
+    private Long idCliente;
+
+    @Column(nullable = false)
+    private Long idGerente;
+
+    @OneToMany(mappedBy = "conta", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Movimentacao> movimentacoes = new ArrayList<>();
+}
---
+++ back-end/ms-conta/src/main/java/com/bantads/ms_conta/model/entity/Movimentacao.java
@@ -0,0 +1,42 @@
+package com.bantads.ms_conta.model.entity;
+
+import com.bantads.ms_conta.model.enums.TipoMovimentacao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movimentacao")
+public class Movimentacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TipoMovimentacao tipo;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal valor;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Conta conta;
+
+    private String contaOrigem;
+    private String contaDestino;
+}
