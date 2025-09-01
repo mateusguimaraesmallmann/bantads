905bd1ccd1b3e072120790d137854bbb258a11d6
Ariane Souza <ari.souza02@outlook.com>
2025-09-01 16:07:06 -0300

    feat[MS-Cliente]: Inserindo DTOs

---
+++ back-end/ms-cliente/src/main/java/com/bantads/ms_cliente/feign/dto/CriarContaDTOIn.java
@@ -0,0 +1,26 @@
+package com.bantads.ms_cliente.feign.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+@Schema(name = "CriarContaDTOIn", description = "Representa os dados de criação de uma conta bancária")
+public final class CriarContaDTOIn {
+
+    @Schema(description = "Saldo inicial da conta", example = "1000.00")
+    private BigDecimal saldoInicial;
+
+    @Schema(description = "Limite de crédito da conta", example = "2000.00")
+    private BigDecimal limite;
+
+    @Schema(description = "Identificador do cliente que terá a conta", example = "101")
+    private Long idCliente;
+
+    @Schema(description = "Identificador do gerente responsável pela conta", example = "5")
+    private Long idGerente;
+
+}
