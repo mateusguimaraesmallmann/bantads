faa77f5c13c8a4df8a2235670f9680429cb69b7b
Mateus Irineu Guimarães Mallmann <mateusmallmann2013@gmail.com>
2025-10-20 08:48:18 -0300

    Iniciada saga de autocadastro
    Iniciada saga de autocadastro

---
+++ back-end/ms-saga/src/main/java/com/bantads/ms_saga/controllers/autocadastro/AutocadastroSagaController.java
@@ -0,0 +1,21 @@
+package com.bantads.ms_saga.controllers.cliente;
+
+import com.bantads.ms_saga.model.dto.input.RemoverGerenteDTOIn;
+import com.bantads.ms_saga.model.dto.output.RemoverGerenteDTOOut;
+import com.bantads.ms_saga.services.gerente.GerenteSagaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autocadastro/saga")
+@RequiredArgsConstructor
+public class AutocadastroSagaController {
+
+    private final AutocadastroSagaService sagaService;
+    
+}
---
+++ back-end/ms-saga/src/main/java/com/bantads/ms_saga/services/autocadastro/AutocadastroSagaService.java
@@ -0,0 +1,47 @@
+package com.bantads.ms_saga.services.cliente;
+
+import com.bantads.ms_saga.client.GerenteClient;
+import com.bantads.ms_saga.model.dto.input.RemoverGerenteDTOIn;
+import com.bantads.ms_saga.model.dto.output.RemoverGerenteDTOOut;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutocadastroSagaService {
+
+    private final ClienteClient ClienteClient;
+
+    public AprovarClienteDTOOut aprovarCliente(String cpf) {
+        
+        AprovarClienteDTOOut response = new AprovarClienteDTOOut();
+        
+        try {
+            ClienteClient.buscarPorCpf(dto.getCpf());
+
+            response.setCpf(cpf);        
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    public CadastroClienteDTOIn cadastrarCliente(CadastroClienteDTOIn dto) {
+        
+        CadastroClienteDTOIn response = new CadastroClienteDTOIn();
+        
+        try {
+            ClienteClient.buscarPorCpf(dto.getCpf());
+
+            response.setCpf(cpf);      
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+    
+}
