457bf6c0cd1849aea3a2282cc615b919e8f6433e
gabisre <gabirech05@gmail.com>
2025-11-01 22:01:07 -0300

    fix para o ms-cliente nao parar

--- back-end/ms-cliente/src/main/java/com/bantads/ms_cliente/MsClienteApplication.java
+++ back-end/ms-cliente/src/main/java/com/bantads/ms_cliente/MsClienteApplication.java
@@ -2,12 +2,13 @@ package com.bantads.ms_cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.bantads.ms_cliente.feign")
 public class MsClienteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsClienteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsClienteApplication.class, args);
+    }
 }
+
