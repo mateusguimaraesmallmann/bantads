7398fe98c5a154f264c80d1d3ecc1fa9a877fb19
Tiago Salles <100172210+Tiaguera021@users.noreply.github.com>
2025-11-10 17:02:11 -0300

    fix: fix dependencies

--- back-end/ms-saga/src/main/java/com/bantads/ms_saga/controllers/cliente/ClienteSagaController.java
+++ back-end/ms-saga/src/main/java/com/bantads/ms_saga/controllers/cliente/ClienteSagaController.java
@@ -1,17 +1,17 @@
 package com.bantads.ms_saga.controllers.cliente;
 
-import com.bantads.ms_saga.model.dto.input.CadastroClienteDTOIn;
-import com.bantads.ms_saga.model.dto.output.AprovarClienteDTOOut;
-import com.bantads.ms_saga.services.gerente.GerenteSagaService;
-import com.bantads.ms_saga.services.cliente.ClienteSagaService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bantads.ms_saga.model.dto.input.CadastroClienteDTOIn;
+import com.bantads.ms_saga.model.dto.output.AprovarClienteDTOOut;
+import com.bantads.ms_saga.services.cliente.ClienteSagaService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/clientes/saga")
--- back-end/ms-saga/src/main/java/com/bantads/ms_saga/controllers/funcionario/FuncionarioSagaController.java
+++ back-end/ms-saga/src/main/java/com/bantads/ms_saga/controllers/funcionario/FuncionarioSagaController.java
@@ -1,22 +1,18 @@
 package com.bantads.ms_saga.controllers.funcionario;
 
-import com.bantads.ms_saga.model.dto.input.CadastroClienteDTOIn;
-import com.bantads.ms_saga.model.dto.output.AprovarClienteDTOOut;
-import com.bantads.ms_saga.services.gerente.GerenteSagaService;
-import com.bantads.ms_saga.services.cliente.ClienteSagaService;
-import com.bantads.ms_saga.model.dto.output.GerenteDTOOut;
-import com.bantads.ms_saga.model.dto.input.GerenteDTOIn;
-
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bantads.ms_saga.model.dto.input.CadastroClienteDTOIn;
+import com.bantads.ms_saga.model.dto.output.AprovarClienteDTOOut;
+import com.bantads.ms_saga.services.cliente.ClienteSagaService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/clientes/saga")
