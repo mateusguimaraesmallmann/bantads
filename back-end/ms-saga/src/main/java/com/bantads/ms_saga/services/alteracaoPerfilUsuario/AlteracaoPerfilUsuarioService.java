36e08fe5aa41246b30366d254cc9cea201b0bad6
Marcel Zimmer <Marcel-zimmer>
2025-11-16 23:00:10 -0300

    fix(altecao-perfil-usuario-saga):removido comentarios no service adicionados no ultimo commit

--- back-end/ms-saga/src/main/java/com/bantads/ms_saga/services/alteracaoPerfilUsuario/AlteracaoPerfilUsuarioService.java
+++ back-end/ms-saga/src/main/java/com/bantads/ms_saga/services/alteracaoPerfilUsuario/AlteracaoPerfilUsuarioService.java
@@ -1,45 +1,45 @@
-// package com.bantads.ms_saga.services.alteracaoPerfilUsuario;
+package com.bantads.ms_saga.services.alteracaoPerfilUsuario;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import com.bantads.ms_saga.client.ClienteClient;
-// import com.bantads.ms_saga.client.ContaClient;
-// import com.bantads.ms_saga.model.dto.input.EditarClienteDTOIn;
-// import com.bantads.ms_saga.model.dto.output.ClienteDTOOut;
-// import com.bantads.ms_saga.model.dto.output.EditarClienteDTOOut;
-// import com.bantads.ms_saga.model.dto.output.RecalcularLimiteDTOOut;
+import com.bantads.ms_saga.client.ClienteClient;
+import com.bantads.ms_saga.client.ContaClient;
+import com.bantads.ms_saga.model.dto.input.EditarClienteDTOIn;
+import com.bantads.ms_saga.model.dto.output.ClienteDTOOut;
+import com.bantads.ms_saga.model.dto.output.EditarClienteDTOOut;
+import com.bantads.ms_saga.model.dto.output.RecalcularLimiteDTOOut;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-// @Service
-// @RequiredArgsConstructor
-// public class AlteracaoPerfilUsuarioService {
+@Service
+@RequiredArgsConstructor
+public class AlteracaoPerfilUsuarioService {
     
-//     private final ClienteClient clienteClient;
-//     private final ContaClient contaClient;
+    private final ClienteClient clienteClient;
+    private final ContaClient contaClient;
     
-//     public EditarClienteDTOOut atualizarCliente(String cpf, EditarClienteDTOIn editarCliente) {
+    public EditarClienteDTOOut atualizarCliente(String cpf, EditarClienteDTOIn editarCliente) {
 
-//         try {
-//             ClienteDTOOut dadosClienteAntigo = clienteClient.buscarPorCpf(cpf);
-//             EditarClienteDTOOut clienteAtualizadoDTO = clienteClient.atualizarCliente(cpf, editarCliente);
+        try {
+            ClienteDTOOut dadosClienteAntigo = clienteClient.buscarPorCpf(cpf);
+            EditarClienteDTOOut clienteAtualizadoDTO = clienteClient.atualizarCliente(cpf, editarCliente);
 
 
-//             if (editarCliente.getSalario() != null && 
-//                 (editarCliente.getSalario().compareTo(dadosClienteAntigo.getSalario()) != 0)) {
-//                 RecalcularLimiteDTOOut dtoRecalcularLimite = new RecalcularLimiteDTOOut(
-//                     dadosClienteAntigo.getId(), 
-//                     editarCliente.getSalario()
-//                 );
-//                 contaClient.recalcularLimite(dtoRecalcularLimite); 
-//             }
-//             return clienteAtualizadoDTO;
+            if (editarCliente.getSalario() != null && 
+                (editarCliente.getSalario().compareTo(dadosClienteAntigo.getSalario()) != 0)) {
+                RecalcularLimiteDTOOut dtoRecalcularLimite = new RecalcularLimiteDTOOut(
+                    dadosClienteAntigo.getId(), 
+                    editarCliente.getSalario()
+                );
+                contaClient.recalcularLimite(dtoRecalcularLimite); 
+            }
+            return clienteAtualizadoDTO;
 
-//         } catch (Exception e) {
-//             return null;
-//         } 
-//     }
-//
-// 
-//}
+        } catch (Exception e) {
+            return null;
+        } 
+    }
+
+
+}
 
